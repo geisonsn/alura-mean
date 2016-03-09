bca538ff6405c44c69e06d26827c4a3662cfb17d
= <geison.sn@gmail.com>
2016-03-09 13:06:58 -0400

    Aula 04 do curso de Mean - parte1: implementação de rota para recuperação de grupos, criação de arquivos específicos para a organização das rotas

--- config/express.js
+++ config/express.js
@@ -3,12 +3,7 @@ var app = express();
 
 app.use(express.static("./public"));
 
-app.get("/v1/fotos", function(req, res) {
-	var fotos = [
-		{_id :1, titulo : "Leão", url : "http://www.fundosanimais.com/Minis/leoes.jpg"},
-		{_id :2, titulo : "Leão 2", url : "http://www.fundosanimais.com/Minis/leoes.jpg"}
-	];
-	res.json(fotos);
-});
+require('../app/routes/foto')(app);
+require('../app/routes/grupo')(app);
 
 module.exports = app;
