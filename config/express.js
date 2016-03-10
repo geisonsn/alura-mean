2d51747748c3cc895243c2185fe5e878c15ee828
Geison Santos <geison.sn@gmail.com>
2016-03-09 21:53:10 -0400

    Aula 04 do curso de Mean - parte4: configundo a biblioteca CONSIGN no arquivo express.js para carregar os arquivos app/api/fotos.js e app/api/grupo.js dinamicamente (sem o require)

--- config/express.js
+++ config/express.js
@@ -4,8 +4,9 @@ var consign = require("consign");
 
 app.use(express.static("./public"));
 
-consign()
-	.include("app/routes")
+consign({cwd : 'app'})
+	.include('api')
+	.then("routes")
 	.into(app);
 
 module.exports = app;
