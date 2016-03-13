991966555c153794f32679b13809c0e32a265485
Geison Santos <geison.sn@gmail.com>
2016-03-12 20:42:30 -0400

    Aula 05 do curso de Mean: implementacão de rota para recuperacão de fotos por ID

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,5 +1,10 @@
 var api = {};
 
+var fotos = [
+		{_id :1, titulo : "Leão", url : "http://www.fundosanimais.com/Minis/leoes.jpg"},
+		{_id :2, titulo : "Leão 2", url : "http://www.fundosanimais.com/Minis/leoes.jpg"}
+	];
+
 api.lista = function(req, res) {
 	var fotos = [
 		{_id :1, titulo : "Leão", url : "http://www.fundosanimais.com/Minis/leoes.jpg"},
@@ -7,5 +12,12 @@ api.lista = function(req, res) {
 	];
 	res.json(fotos);
 };
+
+api.buscaPorId = function(req, res) {
+	var foto = fotos.find(function(foto) {
+		return foto._id == req.params.id;			
+	});
+	res.json(foto);
+}
 
 module.exports = api;
